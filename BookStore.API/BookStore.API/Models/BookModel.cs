579e57de2793cd9a7641418b7d6f22e7b2d838b5
Avinash Dash <20avinashdash@gmail.com>
2021-05-23 23:52:13 +0530

    -Added Required Validation for Book Model

--- BookStore.API/BookStore.API/Models/BookModel.cs
+++ BookStore.API/BookStore.API/Models/BookModel.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+//It is used for validation like particular filed is required 
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.API.Models
 {
     public class BookModel
     {
         public int Id { get; set; }
+        //The following Annotaion will make tile Required and add custom error message
+        [Required(ErrorMessage = "Please Add title property")]
         public string Title { get; set; }
         public string Description { get; set; }
     }
